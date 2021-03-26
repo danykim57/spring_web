a51cbfe9145413e5c5a807dbd9627e2ffa68de20
Yeongin Kim <fhtmah12@gmail.com>
2021-03-26 17:02:20 +0900

    edited

--- src/main/java/com/dan/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/dan/book/springboot/domain/posts/PostsRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
