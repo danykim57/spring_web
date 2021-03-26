cc001cd36efcdafaafe1cd64958f3ca1a899b9ea
Yeongin Kim <fhtmah12@gmail.com>
2021-03-26 11:15:31 +0900

    property change

--- src/main/java/com/dan/book/springboot/Application.java
+++ src/main/java/com/dan/book/springboot/Application.java
@@ -1,14 +1,8 @@
 package com.dan.book.springboot;
 
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.PumpStreamHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.apache.commons.exec.CommandLine;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 @EnableJpaAuditing
 @SpringBootApplication
