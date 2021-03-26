caa6c8b961f5cf3bcf104b33dbaaa19300fd1408
Yeongin Kim <fhtmah12@gmail.com>
2021-03-26 15:15:49 +0900

    updated

--- src/main/java/com/dan/book/springboot/config/JpaConfig.java
+++ src/main/java/com/dan/book/springboot/config/JpaConfig.java
@@ -1,7 +1,9 @@
 package com.dan.book.springboot.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing // JPA Auditing 활성화
-public class JpaConfig {}
+@EnableJpaAuditing
+public class JpaConfig {
+}
